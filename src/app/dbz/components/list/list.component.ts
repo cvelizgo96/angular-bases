bea4d9d2e9a88410cb47631a41623debdb71101e
Carlos <cveliz.go@gmail.com>
2024-09-19 13:47:07 -0500

    Comunicación entre el main-page y los componentes add-character y list mediante EventEmitter

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'dbz-list',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+
+  @Input()
+  public characterList: Character[] = [{
+    name: 'Trunks',
+    power: 5000
+  }];
+
+  @Output()
+  public onDelete:EventEmitter<number> = new EventEmitter();
+
+  onDeleteCharacter(index:number):void{
+    //TODO: Emitir el ID del personaje
+      this.onDelete.emit(index);
+    }
 
 }
